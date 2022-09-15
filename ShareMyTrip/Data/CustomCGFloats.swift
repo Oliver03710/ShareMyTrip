9480714fd0d06f2ccce26dfd8433d2c8a8d0126c
Oliver <jhee2619@gmail.com>
2022-09-15 16:02:05 +0900

    커스텀 생성

---
+++ ShareMyTrip/Data/CustomCGFloats.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCGFloats.swift
+//  ShareMyTrip
+//
+//  Created by Junhee Yoon on 2022/09/14.
+//
+
+import Foundation
+
+struct CustomCGFloats {
+    private init() { }
+    
+    static let settings: CGFloat = 50
+
+}
