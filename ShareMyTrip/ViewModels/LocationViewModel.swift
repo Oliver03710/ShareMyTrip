1c172b2ad003777914863fee389676b1bc945745
Oliver <jhee2619@gmail.com>
2022-09-15 16:06:41 +0900

    ViewModel 테스트

---
+++ ShareMyTrip/ViewModels/LocationViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  LocationViewModel.swift
+//  ShareMyTrip
+//
+//  Created by Junhee Yoon on 2022/09/13.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationViewModel {
+    
+    // MARK: - Properties
+    
+    var email: Observable<String> = Observable("")
+        
+    
+    // MARK: - Helper Functions
+    
+
+    
+}
