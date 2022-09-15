8a0c6844503718db628472c1ee8b1b7bbeaf3011
Oliver <jhee2619@gmail.com>
2022-09-15 16:03:23 +0900

    여행기록 뷰컨트롤러 생성

---
+++ ShareMyTrip/Histories/HistoriesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HistoriesViewController.swift
+//  ShareMyTrip
+//
+//  Created by Junhee Yoon on 2022/09/13.
+//
+
+import UIKit
+
+class HistoriesViewController: BaseViewController {
+
+    // MARK: - Init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    // MARK: - Helper Functions
+    
+    override func configureUI() {
+        
+    }
+}
