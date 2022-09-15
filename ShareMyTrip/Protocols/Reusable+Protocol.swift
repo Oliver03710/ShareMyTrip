d8a92593cc14fdf8f17d103b90a22e9330f043e1
Oliver <jhee2619@gmail.com>
2022-09-15 16:05:28 +0900

    reusable Identifier 생성

---
+++ ShareMyTrip/Protocols/Reusable+Protocol.swift
@@ -0,0 +1,24 @@
+//
+//  Reusable+Protocol.swift
+//  ShareMyTrip
+//
+//  Created by Junhee Yoon on 2022/09/14.
+//
+
+import UIKit
+
+public protocol ReusableViewProtocol {
+    static var reuseIdentifier: String { get }
+}
+
+extension UIViewController: ReusableViewProtocol {
+    public static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UITableViewCell: ReusableViewProtocol {
+    public static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
