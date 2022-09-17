b6072a8c967ba9f13a11f30a048590b9a0823294
Oliver <jhee2619@gmail.com>
2022-09-17 16:01:08 +0900

    여행동료 추가 기능 구현

---
+++ ShareMyTrip/ViewModels/CompanionViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  CompanionViewModel.swift
+//  ShareMyTrip
+//
+//  Created by Junhee Yoon on 2022/09/17.
+//
+
+import Foundation
+
+class CompanionViewModel {
+    
+    // MARK: - Properties
+    
+    var person: Observable<[String]> = Observable([])
+        
+    
+    // MARK: - Helper Functions
+    
+//    func checkSections(section: Int) -> Int {
+//        switch section {
+//        case 0: return person.value.count
+//        default: return 1
+//        }
+//    }
+    
+}
