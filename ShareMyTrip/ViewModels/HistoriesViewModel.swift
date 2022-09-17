305883130408b7042762ac8a59306056fb1640f9
Oliver <jhee2619@gmail.com>
2022-09-18 03:15:31 +0900

    여행 히스토리 뷰모델 생성

---
+++ ShareMyTrip/ViewModels/HistoriesViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  HistoriesViewModel.swift
+//  ShareMyTrip
+//
+//  Created by Junhee Yoon on 2022/09/17.
+//
+
+import Foundation
+
+final class HistoriesViewModel {
+    
+    // MARK: - Properties
+    
+    var destinations: Observable<[String]> = Observable([])
+    
+    
+    // MARK: - Helper Functions
+    
+}
