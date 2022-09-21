9a6debe9395985487c14c7649413c9edd87f31dc
Oliver <jhee2619@gmail.com>
2022-09-22 02:18:28 +0900

    삭제 분기처리에 따른 코드 수정

--- ShareMyTrip/Extensions/AlertMessage+Extension.swift
+++ ShareMyTrip/Extensions/AlertMessage+Extension.swift
@@ -9,14 +9,22 @@ import UIKit
 
 extension UIViewController {
     
-    func showAlertMessage(buttonText: String, alertTitle: String?, target: Observable<[String]>) {
+    func showAlertMessage(buttonText: String, alertTitle: String?, tableView: UITableView, buttonType: ButtonType) {
         
         let alert = UIAlertController(title: alertTitle, message: nil, preferredStyle: .alert)
-        alert.addTextField { textField in
-            textField.attributedPlaceholder = NSAttributedString(string: "너! 내 동료가 돼라!", attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray])
+        if buttonType == .addButton {
+            alert.addTextField { textField in
+                textField.attributedPlaceholder = NSAttributedString(string: "너! 내 동료가 돼라!", attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray])
+            }
         }
-        let confirm = UIAlertAction(title: buttonText, style: .default) { _ in
-            target.value.append(alert.textFields?.first?.text ?? "")
+        let confirm = UIAlertAction(title: buttonText, style: buttonType == .addButton ? .default : .destructive) { _ in
+            if buttonType == .addButton {
+                CompanionsRepository.standard.addItem(companion: alert.textFields?.first?.text ?? "")
+            } else {
+                CompanionsRepository.standard.deleteAllItem()
+            }
+            CompanionsRepository.standard.fetchRealmData()
+            tableView.reloadData()
         }
         
         let cancel = UIAlertAction(title: "취소", style: .cancel)
@@ -26,17 +34,25 @@ extension UIViewController {
         
     }
     
-    func showAlertMessage(buttonText: String, alertTitle: String?, completionHandler: @escaping () -> Void) {
+    func showAlertMessage(deleteLast: @escaping () -> Void, deleteAllItem: @escaping () -> Void, deleteAllWithTransition: @escaping () -> Void) {
         
-        let alert = UIAlertController(title: alertTitle, message: nil, preferredStyle: .alert)
+        let alert = UIAlertController(title: "삭제할 내용을 선택해주세요.", message: nil, preferredStyle: .actionSheet)
         
-        let delete = UIAlertAction(title: buttonText, style: .destructive) { _ in
-            completionHandler()
+        let deleteLast = UIAlertAction(title: "마지막 목적지 삭제", style: .destructive) { _ in
+            deleteLast()
+        }
+        
+        let deleteAllItem = UIAlertAction(title: "목적지 전체 삭제", style: .destructive) { _ in
+            deleteAllItem()
+        }
+        
+        let deleteAllWithTransition = UIAlertAction(title: "여행 삭제 후 초기화면으로 이동", style: .destructive) { _ in
+            deleteAllWithTransition()
         }
         
         let cancel = UIAlertAction(title: "취소", style: .cancel)
         
-        [cancel, delete].forEach { alert.addAction($0) }
+        [cancel, deleteLast, deleteAllItem, deleteAllWithTransition].forEach { alert.addAction($0) }
         self.present(alert, animated: true)
         
     }
