271d9d6d3ea060781747e147bb4ddcf799a1c505
Oliver <jhee2619@gmail.com>
2022-09-22 02:19:57 +0900

    여행동료 데이터 관리에 따른 Realm 추가

---
+++ ShareMyTrip/RealmData/Companions+Realm.swift
@@ -0,0 +1,22 @@
+//
+//  Companions+Realm.swift
+//  ShareMyTrip
+//
+//  Created by Junhee Yoon on 2022/09/21.
+//
+
+import Foundation
+
+import RealmSwift
+
+class Companions: Object {
+    @Persisted var companion: String
+    
+    @Persisted(primaryKey: true) var objectId: ObjectId
+    
+    convenience init(companion: String) {
+        self.init()
+        self.companion = companion
+    }
+    
+}
---
+++ ShareMyTrip/RealmData/Companions+Repository.swift
@@ -0,0 +1,72 @@
+//
+//  Companions+Repository.swift
+//  ShareMyTrip
+//
+//  Created by Junhee Yoon on 2022/09/21.
+//
+
+import Foundation
+
+import RealmSwift
+
+enum ButtonType {
+    case addButton, deleteButton
+}
+
+private protocol CompanionsRepositoryType: AnyObject {
+    func addItem(companion: String)
+    func deleteSpecificItem(item: Companions)
+    func deleteAllItem()
+    func fetchRealmData()
+}
+
+final class CompanionsRepository: CompanionsRepositoryType {
+
+    private init() { }
+    
+    static let standard = CompanionsRepository()
+    
+    // MARK: - Init
+    
+    let localRealm = try! Realm()
+    var tasks: Results<Companions>!
+    
+    
+    // MARK: - Helper Functions
+    
+    func addItem(companion: String) {
+        let task = Companions(companion: companion)
+        do {
+            try localRealm.write {
+                localRealm.add(task)
+            }
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    func deleteSpecificItem(item: Companions) {
+        do {
+            try localRealm.write {
+                localRealm.delete(item)
+            }
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    func deleteAllItem() {
+        do {
+            try localRealm.write {
+                localRealm.deleteAll()
+            }
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    func fetchRealmData() {
+        tasks = localRealm.objects(Companions.self)
+    }
+    
+}
