2fea35b562927de6013a802527c28fb7e60efc07
Oliver <jhee2619@gmail.com>
2022-09-22 02:15:09 +0900

    데이터 추가

--- ShareMyTrip/Data/CustomCGFloats.swift
+++ ShareMyTrip/Data/CustomCGFloats.swift
@@ -8,5 +8,7 @@
 import Foundation
 
 struct CustomCGFloats {
-    static let settings: CGFloat = 50
+    static let settingView: CGFloat = 50
+    static let searchingView: CGFloat = 60
+    static let destinationView: CGFloat = 60
 }
