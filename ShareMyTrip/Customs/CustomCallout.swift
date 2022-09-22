1d528a4711dc1367d90daea4a81c451d0b00916d
Oliver <jhee2619@gmail.com>
2022-09-23 00:36:56 +0900

    Custom Callout 내부 코드 정리

--- ShareMyTrip/Customs/CustomCallout.swift
+++ ShareMyTrip/Customs/CustomCallout.swift
@@ -9,9 +9,7 @@ import MapKit
 
 final class Callout: UIView {
     
-    private let titleLabel = UILabel(frame: .zero)
     private let subtitleLabel = UILabel(frame: .zero)
-    private let imageView = UIImageView(frame: .zero)
     private let annotation: MKAnnotation
     
     init(annotation: MKAnnotation) {
@@ -26,44 +24,21 @@ final class Callout: UIView {
     
     private func setupView() {
         translatesAutoresizingMaskIntoConstraints = false
-        setupTitle()
         setupSubtitle()
-        setupImageView()
-    }
-    
-    private func setupTitle() {
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 20)
-        titleLabel.text = annotation.title as? String
-        addSubview(titleLabel)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-        titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
     }
     
     private func setupSubtitle() {
         subtitleLabel.font = UIFont.systemFont(ofSize: 14)
         subtitleLabel.textColor = .gray
-        subtitleLabel.text = "위치정보"
+        subtitleLabel.text = annotation.subtitle as? String
         addSubview(subtitleLabel)
         subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
-        subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8).isActive = true
+        subtitleLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
         subtitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
         subtitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
-    }
-    
-    private func setupImageView() {
-        imageView.image = UIImage(systemName: "applelogo")
-        imageView.contentMode = .scaleAspectFill
-        imageView.clipsToBounds = true
-        addSubview(imageView)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 8).isActive = true
-        imageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-        imageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
-        imageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
-        imageView.widthAnchor.constraint(equalToConstant: 280).isActive = true
+        subtitleLabel.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        subtitleLabel.heightAnchor.constraint(equalToConstant: 14).isActive = true
+        subtitleLabel.widthAnchor.constraint(equalToConstant: 350).isActive = true
     }
     
 }
