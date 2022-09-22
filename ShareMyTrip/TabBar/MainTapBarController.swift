fa5a7a45f6f463727bb44a2961a56a035962f261
Oliver <jhee2619@gmail.com>
2022-09-23 03:33:16 +0900

    코드 수정

--- ShareMyTrip/TabBar/MainTapBarController.swift
+++ ShareMyTrip/TabBar/MainTapBarController.swift
@@ -26,7 +26,7 @@ final class MainTapBarController: UITabBarController {
         viewControllers = [
             createNavController(for: MapViewController(), title: TabBarTitles.mapVC.rawValue, image: UIImage(systemName: TabBarImages.mapVC.rawValue)),
             createNavController(for: DestinationsViewController(), title: TabBarTitles.destinationVC.rawValue, image: UIImage(systemName: TabBarImages.destinationVC.rawValue)),
-            createNavController(for: SharingListViewController(), title: TabBarTitles.sharingListVC.rawValue, image: UIImage(systemName: TabBarImages.sharingListVC.rawValue)),
+            createNavController(for: CompanionsViewController(), title: TabBarTitles.sharingListVC.rawValue, image: UIImage(systemName: TabBarImages.sharingListVC.rawValue)),
             createNavController(for: HistoriesViewController(), title: TabBarTitles.historiesVC.rawValue, image: UIImage(systemName: TabBarImages.historiesVC.rawValue)),
             createNavController(for: SettingsViewController(), title: TabBarTitles.SettingsVC.rawValue, image: UIImage(systemName: TabBarImages.SettingsVC.rawValue))
         ]
