542d5900ac541f6f936f0f8a7f3dea6396164309
Oliver <jhee2619@gmail.com>
2022-10-04 05:03:59 +0900

    이미지 추가에 따른 코드 추가

--- ShareMyTrip/Data/ImageData.swift
+++ ShareMyTrip/Data/ImageData.swift
@@ -11,6 +11,10 @@ enum CharacterImage: String, CaseIterable {
     case main = "travelBearMainBlue"
     case smile = "smileFace"
     case crying = "cryingFace"
+    case searchImage
+    case destinationImage
+    case tripHistoryImage
+    case companionImage
 }
 
 enum startButtonImage: String, CaseIterable {
@@ -18,6 +22,28 @@ enum startButtonImage: String, CaseIterable {
     case tapped = "travelBearMainButtonTapped"
 }
 
+enum finishButtonImage: String, CaseIterable {
+    case untapped = "finishTripButton"
+    case tapped = "finishTripButtonTapped"
+}
+
+enum restoreButtonImage: String, CaseIterable {
+    case fetchFileImage
+}
+
 enum IconImage: String, CaseIterable {
     case info, phone, location
+}
+
+enum TabBarButtonImage: String, CaseIterable {
+    case mapButtonUntapped
+    case mapButtonTapped
+    case pinButtonUntapped
+    case pinButtonTapped
+    case companionButtonTapped
+    case companionButtonUntapped
+    case listButtonTapped
+    case listButtonUntapped
+    case extraButtonTapped
+    case extraButtonUntapped
 }
