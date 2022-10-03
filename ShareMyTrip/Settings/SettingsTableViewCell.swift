29df2a63d7568229c5440b33cfb252c3e89d0b3c
Oliver <jhee2619@gmail.com>
2022-10-04 05:09:57 +0900

    더보기 뷰 컨트롤러 세부 UI 구성

--- ShareMyTrip/Settings/SettingsTableViewCell.swift
+++ ShareMyTrip/Settings/SettingsTableViewCell.swift
@@ -15,6 +15,20 @@ final class SettingsTableViewCell: BaseTableViewCell {
     
     private let settingLabel: BaseLabel = {
         let label = BaseLabel(boldStyle: .regular, fontSize: 15, text: nil)
+        label.textColor = .black
+        return label
+    }()
+    
+    private let nextImageView: UIImageView = {
+        let iv = UIImageView(image: UIImage(systemName: "chevron.right"))
+        iv.contentMode = .scaleAspectFit
+        iv.tintColor = .black
+        return iv
+    }()
+    
+    private let versionLabel: BaseLabel = {
+        let label = BaseLabel(boldStyle: .regular, fontSize: 15, text: nil)
+        label.textColor = .black
         return label
     }()
     
@@ -23,7 +37,7 @@ final class SettingsTableViewCell: BaseTableViewCell {
     
     private override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
+        backgroundColor = UIColor(red: 241/255, green: 241/255, blue: 241/255, alpha: 1)
     }
     
     required init?(coder: NSCoder) {
@@ -33,20 +47,49 @@ final class SettingsTableViewCell: BaseTableViewCell {
     
     // MARK: - Helper Functions
     
-    override func configureUI() {
-        [settingLabel].forEach { self.addSubview($0) }
-    }
-    
     override func setConstraints() {
+        [settingLabel, nextImageView, versionLabel].forEach { contentView.addSubview($0) }
         
         settingLabel.snp.makeConstraints { make in
-            make.edges.equalTo(self.safeAreaLayoutGuide).inset(16)
+            make.directionalVerticalEdges.leading.equalTo(contentView.safeAreaLayoutGuide).inset(16)
+            make.width.equalTo(contentView.snp.width).dividedBy(2)
+        }
+        
+        nextImageView.snp.makeConstraints { make in
+            make.directionalVerticalEdges.trailing.equalTo(contentView.safeAreaLayoutGuide).inset(16)
+            make.width.equalTo(nextImageView.snp.height)
+        }
+
+        versionLabel.snp.makeConstraints { make in
+            make.directionalVerticalEdges.trailing.equalTo(contentView.safeAreaLayoutGuide).inset(16)
+            make.width.equalTo(versionLabel.snp.height).multipliedBy(2)
         }
         
     }
     
-    func setCellComponents(index: Int) {
-        settingLabel.text = SettingsLabelTexts.allCases[index].rawValue
+    func setCellComponents(indexPath: IndexPath, isText: Bool = false) {
+        versionLabel.isHidden = !isText
+        nextImageView.isHidden = isText
+        switch indexPath {
+        case [0, 0]:
+            settingLabel.text = SettingsDataTexts.allCases[indexPath.row].rawValue
+        case [0, 1]:
+            settingLabel.text = SettingsDataTexts.allCases[indexPath.row].rawValue
+        case [1, 0]:
+            settingLabel.text = SettingsToDevTexts.allCases[indexPath.row].rawValue
+        case [1, 1]:
+            settingLabel.text = SettingsToDevTexts.allCases[indexPath.row].rawValue
+        case [2, 0]:
+            settingLabel.text = SettingsInfoTexts.allCases[indexPath.row].rawValue
+        case [2, 1]:
+            settingLabel.text = SettingsInfoTexts.allCases[indexPath.row].rawValue
+            versionLabel.isHidden = isText
+            nextImageView.isHidden = !isText
+            versionLabel.text = "1.0.0"
+        default:
+            break
+        }
+        
     }
     
 }
