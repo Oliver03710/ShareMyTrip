c763459bf2f61e339d61028fd3c55675614d6554
Oliver <jhee2619@gmail.com>
2022-10-04 04:55:30 +0900

    각 탭별로 색 지정

--- ShareMyTrip/TabBar/MainTapBarController.swift
+++ ShareMyTrip/TabBar/MainTapBarController.swift
@@ -15,7 +15,6 @@ final class MainTapBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        tabBar.tintColor = .label
         self.modalPresentationStyle = .fullScreen
         configureTabBars()
     }
@@ -25,19 +24,30 @@ final class MainTapBarController: UITabBarController {
     
     private func configureTabBars() {
         viewControllers = [
-            createNavController(for: MapViewController(), title: TabBarTitles.mapVC.rawValue, image: UIImage(systemName: TabBarImages.mapVC.rawValue)),
-            createNavController(for: DestinationsViewController(), title: TabBarTitles.destinationVC.rawValue, image: UIImage(systemName: TabBarImages.destinationVC.rawValue)),
-            createNavController(for: CompanionsViewController(), title: TabBarTitles.sharingListVC.rawValue, image: UIImage(systemName: TabBarImages.sharingListVC.rawValue)),
-            createNavController(for: HistoriesViewController(), title: TabBarTitles.historiesVC.rawValue, image: UIImage(systemName: TabBarImages.historiesVC.rawValue)),
-            createNavController(for: SettingsViewController(), title: TabBarTitles.SettingsVC.rawValue, image: UIImage(systemName: TabBarImages.SettingsVC.rawValue))
+            createNavController(for: MapViewController(), title: TabBarTitles.mapVC.rawValue, image: UIImage(named: TabBarButtonImage.mapButtonUntapped.rawValue), imageTapped: UIImage(named: TabBarButtonImage.mapButtonTapped.rawValue), backgroundColor: UIColor(red: 106/255, green: 216/255, blue: 0/255, alpha: 1)),
+            createNavController(for: DestinationsViewController(), title: TabBarTitles.destinationVC.rawValue, image: UIImage(named: TabBarButtonImage.pinButtonUntapped.rawValue), imageTapped: UIImage(named: TabBarButtonImage.pinButtonTapped.rawValue), backgroundColor: UIColor(red: 64/255, green: 211/255, blue: 244/255, alpha: 1)),
+            createNavController(for: CompanionsViewController(), title: TabBarTitles.sharingListVC.rawValue, image: UIImage(named: TabBarButtonImage.companionButtonUntapped.rawValue), imageTapped: UIImage(named: TabBarButtonImage.companionButtonTapped.rawValue), backgroundColor: UIColor(red: 255/255, green: 162/255, blue: 109/255, alpha: 1)),
+            createNavController(for: HistoriesViewController(), title: TabBarTitles.historiesVC.rawValue, image: UIImage(named: TabBarButtonImage.listButtonUntapped.rawValue), imageTapped: UIImage(named: TabBarButtonImage.listButtonTapped.rawValue), backgroundColor: UIColor(red: 226/255, green: 162/255, blue: 109/255, alpha: 1)),
+            createNavController(for: SettingsViewController(), title: TabBarTitles.SettingsVC.rawValue, image: UIImage(named: TabBarButtonImage.extraButtonUntapped.rawValue), imageTapped: UIImage(named: TabBarButtonImage.extraButtonTapped.rawValue), backgroundColor: UIColor(red: 183/255, green: 183/255, blue: 183/255, alpha: 1))
         ]
     }
     
-    private func createNavController(for rootViewController: UIViewController, title: String, image: UIImage?) -> UIViewController {
+    private func createNavController(for rootViewController: UIViewController, title: String, image: UIImage?, imageTapped: UIImage?, backgroundColor: UIColor?) -> UIViewController {
+        
         let navController = UINavigationController(rootViewController: rootViewController)
-        navController.tabBarItem.title = title
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = backgroundColor
+        navController.navigationBar.scrollEdgeAppearance = appearance
+        
+        navController.navigationBar.tintColor = .white
         navController.tabBarItem.image = image
+        navController.tabBarItem.selectedImage = imageTapped
+        navController.tabBarItem.imageInsets = UIEdgeInsets(top: 10, left: 0, bottom: -10, right: 0)
+        
         rootViewController.navigationItem.title = title
+        
         return navController
     }
     
