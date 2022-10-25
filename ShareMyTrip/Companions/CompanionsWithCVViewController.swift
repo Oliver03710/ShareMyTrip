a3ca3592c62c7f724a4513ff9d5b518888c7f7b6
Oliver03710 <jhee2619@gmail.com>
2022-10-25 15:27:11 +0900

    Create Collection View With Compositional Layout & Diffable DataSource

---
+++ ShareMyTrip/Companions/CompanionsWithCVViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CompanionsWithCVViewController.swift
+//  ShareMyTrip
+//
+//  Created by Junhee Yoon on 2022/10/25.
+//
+
+import UIKit
+
+class CompanionsWithCVViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    // MARK: - Helper Functions
+    
+
+}
